26d5780a9129bed563339587fc6a48d6aa206504
Carlos Silva <Carlosteixeira.tc@hotmail.com>
2020-09-05 19:48:56 -0300

    List the itens store
    List the itens store

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,20 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productsTotal = product.price * product.quantity;
+      return accumulator + productsTotal;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
-
-    return 0;
+    const itens = products.reduce((accumulator, product) => {
+      const productsTotal = product.quantity;
+      return accumulator + productsTotal;
+    }, 0);
+    return itens;
   }, [products]);
 
   return (
